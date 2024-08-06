8e43f3431a6382d8fd8f1cb6870de12d5b5620c4
sahil nandaniya <snandaniya@hhaexchange.com>
2024-08-06 16:04:33 +0530

    Logout Implemented

--- Flipkart/MVVM/ViewModels/AppShellViewModel.cs
+++ Flipkart/MVVM/ViewModels/AppShellViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Flipkart.Services;
 
 namespace Flipkart.MVVM.ViewModels;
@@ -11,10 +12,22 @@ public partial class AppShellViewModel: ObservableObject
     [ObservableProperty]
     public static string userName = "Login & Signup";
 
+    private AuthService authService;
+
     public AppShellViewModel(AuthService authService)
     {
+        this.authService = authService;
         IsUserLoggedIn = authService.IsUserLoggedIn;
         if(IsUserLoggedIn)
             UserName = "UserName";
+    }
+
+    [RelayCommand]
+    public void Logout()
+    {
+        SecureStorage.Default.RemoveAll();
+        authService.IsUserLoggedIn = IsUserLoggedIn = false;
+        UserName = "Login & Signup";
+        Shell.Current.FlyoutIsPresented = false;
     }
 }
